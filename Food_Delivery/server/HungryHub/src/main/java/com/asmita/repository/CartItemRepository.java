1092b75505572aeac63e127b10531a5fd1408882
Asmita Lodhavat <140889117+W2-80634@users.noreply.github.com>
2024-03-16 18:40:44 +0530

    Delete Food_Delivery/server directory

--- Food_Delivery/server/HungryHub/src/main/java/com/asmita/repository/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.asmita.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.asmita.model.CartItem;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long>{
-
-}
--- Food_Delivery/server/HungryHub/src/main/java/com/asmita/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.asmita.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.asmita.model.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
-
-}
